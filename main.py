55aef8b5f669aada827becb69a07af3d59f4a236
Manit Gupta <82293878+manit-cool@users.noreply.github.com>
2024-08-16 18:47:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+import streamlit as st
+from datetime import datetime
+
+#function to display the current time
+
+def display_time():
+  now = datetime.now()
+  current_time = now.strftime("%H:%M:%S")
+  return current_time
+
+#Sidebar for navigation
+
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Home, "About"])
+
+#Homepage
+
+if page=="Home":
+  st.title("Home")
+  st.subheader("Welcome to my home page")
+
+  #Display current time
+  st.write(f"### Current time: {display_time()}")
+
+#About page
+
+elif page == "About":
+  st.title("About")
+  st.subheader("Hola! Made by Manit Gupta")
+
+
+
+
+
+
+
+
+
+
+
+
 
